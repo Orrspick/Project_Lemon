d555f3126aedde0d26418be4ce5d99cc6674f1ae
Orrspick <49950513+Orrspick@users.noreply.github.com>
2020-04-23 02:42:58 +0900

    일부 추가
    캐릭터 모션 동작 코딩
    캐릭터 점프 동작 코딩 및 중력제어 테스트
    캐릭터 최대 속도 코드 추가
    임시로 타일맵 추가(사용하지는 않음)

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,19 +4,51 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    Rigidbody2D rigid;
+    public float maxSpeed; //최대 속도 제한
+    public float jumpPower; // 점프속도
+    Rigidbody2D rigid; //리지드보디 불러오기
+    SpriteRenderer spriteRenderer; // 스프라이트2D 제어
+    Animator anim;
 
-    // Start is called before the first frame update
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    void Update() //단발적인 키 입력
     {
-        float h = Input.GetAxisRaw("Horizontal");
+        //점프
+        if(Input.GetButtonDown("Jump"))
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
 
+        // 키를 떗을떄 속도 감속
+        if (Input.GetButtonUp("Horizontal")) {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+
+        //스프라이트 플립 변경
+        if(Input.GetButtonDown("Horizontal")) {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+
+        // 에니메이션
+        if(Mathf.Abs(rigid.velocity.x) < 0.3)
+            anim.SetBool("Player_walk", false);
+        else
+            anim.SetBool("Player_walk", true);
+    }
+
+    void FixedUpdate() //지속적인 키 입력
+    {
+        //조작
+        float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed) //오른쪽 최대 속도 제한
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed*(-1)) //왼쪽 최대 속도 제한
+            rigid.velocity = new Vector2(maxSpeed*(-1), rigid.velocity.y);
     }
 }
